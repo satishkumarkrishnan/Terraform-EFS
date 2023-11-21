bfc10b07976b3338a591591b57d05cfb6af9cef2
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-21 11:54:13 +0530

    Adding TF conf files for EFS

--- main.tf
+++ main.tf
@@ -8,3 +8,30 @@ terraform {
     }
   }
 }
+
+module "asg" {
+  source="git@github.com:satishkumarkrishnan/terraform-aws-asg.git?ref=main"  
+}
+
+module "kms" {
+  source="git@github.com:satishkumarkrishnan/Terraform-KMS.git?ref=main"  
+}
+
+#Resource  code for creating EFS 
+resource "aws_efs_file_system" "tokyo_efs" {
+  creation_token = "tokyo-token"
+  kms_key_id     = module.kms.kms_arn
+
+  tags = {
+    Name = "TokyoEFS"
+  }
+}
+
+#Adding lifecycle Policy
+resource "aws_efs_file_system" "foo_with_lifecyle_policy" {
+  creation_token = "tokyo-token"
+
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+}
