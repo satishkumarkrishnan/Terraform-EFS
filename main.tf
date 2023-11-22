606c8010b7669271c4f437a1fa1cf3c69a95fa72
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-22 11:02:15 +0530

    Adding  the  depend on condition in lifecycle

--- main.tf
+++ main.tf
@@ -31,7 +31,8 @@ resource "aws_efs_file_system" "tokyo_efs" {
 
 #Adding lifecycle Policy
 resource "aws_efs_file_system" "foo_with_lifecyle_policy" {
-  creation_token = "tokyo_token"  
+  creation_token = "tokyo_token" 
+  depends_on = [module.asg]
 
   lifecycle_policy {
     transition_to_ia = "AFTER_7_DAYS"    
