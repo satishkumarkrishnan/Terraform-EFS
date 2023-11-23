0b7f2f25c43fe58b617ed2c7cd9d18b2927e7432
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-23 11:18:49 +0530

    Added VPC SG printed

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,10 @@ output "asg_vpc_subnet" {
   value = module.asg.vpc_subnet
 }
 
-output "asg_efs_" {
+output "asg_efs_id" {
   value = aws_efs_file_system.tokyo_efs.id
 }
 
+output "asg_efs_vpc_sg" {
+  value = aws_efs_mount_target.alpha.security_groups
+}
