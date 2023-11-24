2a9e2a4e3d8edd5c931b59d61a6995f49152a6d8
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-11-24 12:11:36 +0530

    Adding the remote provisioner

--- main.tf
+++ main.tf
@@ -79,41 +79,26 @@ resource "aws_efs_file_system_policy" "policy" {
   policy         = data.aws_iam_policy_document.policy.json
 }
 
-/*resource "null_resource" "configure_nfs" {
+resource "null_resource" "configure_nfs" {
   depends_on = [ aws_efs_access_point.tokyo_EFS_accesspoint, aws_efs_mount_target.tokyo_EFS_mount ]
   connection {
     type     = "ssh"
-    user     = "ubuntu"
+    user     = "ec2-user"
     private_key = module.asg.private_key
     host     = module.asg.instance_id
   }
   provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update -y",
-      "sudo apt-get install nfs-common -y",
-      "sudo apt-get install python3.8 -y",
-      "sudo apt-get install python3-pip -y",
-      "python --version",
-      "python3 --version",
-      "echo ${aws_efs_file_system.tokyo_efs.dns_name}",
-      "ls -la",
-      "pwd",
-      "sudo mkdir -p mount-point",
-      "ls -la",
-      "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.tokyo_efs.dns_name}:/ mount-point",
-      "ls",
-      "sudo chown -R ubuntu.ubuntu mount-point",      
-      "cd mount-point",
-      "ls",
-      "mkdir access",      
-     # "sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1",
-     # "sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2",
-     # "printf '2\n' | sudo update-alternatives --config python3",
-     # "pwd",
-     # "ls -la",
-     # "echo 'Python version:'",
-     # "python3 --version",
-     # "pip3 install --upgrade --target ./access/ numpy --system"
-    ]
-  }
-}*/
+inline = [
+"sudo yum install httpd php git -y -q ",
+"sudo systemctl start httpd",
+"sudo systemctl enable httpd",
+"sudo yum install nfs-utils -y -q ", # Amazon ami has pre installed nfs utils
+# Mounting Efs 
+"sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.tokyo_efs.dns_name}:/  /var/www/html",
+# Making Mount Permanent
+"echo ${aws_efs_file_system.tokyo_efs.dns_name}:/ /var/www/html nfs4 defaults,_netdev 0 0  | sudo cat >> /etc/fstab " ,
+"sudo chmod go+rw /var/www/html",
+"sudo git clone https://github.com/Apeksh742/EC2_instance_with_terraform.git /var/www/html",
+  ]
+ }
+}
